e8dc59155bb66a90a8182b7f1b9b153c99e9a98f
Zeeshan Saeed <zs.zeeshansaeed22@gmail.com>
2023-02-21 10:53:30 +0500

    Adding limit to same endpoint

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
-/* GET CDR. */
+/* GET A Single Block. */
 router.get("/mevBlock", async function (req, res, next) {
   try {
+    let limit = "";
+    if (req.query.count) limit = `&limit=${req.query.count}`;
     const response = await axios.get(
-      `http://localhost:3000/zm_mev_transaction?block_number=eq.${req.query.block_number}`
+      `http://localhost:3000/zm_mev_transaction?block_number=eq.${req.query.block_number}` +
+        limit
     );
     const responseData = response.data;
     res.json(responseData);
