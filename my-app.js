49584b169e09ae2f2228a7886b792b6ef1ea9981
Zeeshan Saeed <zs.zeeshansaeed22@gmail.com>
2023-03-15 02:01:10 +0500

    Logo Modified

--- my-app.js
+++ my-app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const fs = require("fs");
 
 require("dotenv").config();
 const port = process.env.Server_Port || 80;
@@ -11,6 +12,11 @@ const router = require(path.join(__dirname, "./src/routes/routes"));
 
 const swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
+
+const swaggerOptions = {
+  customCss:
+    ".topbar-wrapper img {content:url('https://data.zeromev.org/logo');}",
+};
 
 app.use(express.json());
 app.use(
@@ -24,6 +30,21 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to API !!!" });
 });
 
+app.get("/logo", async function (req, res, next) {
+  try {
+    fs.access(`./zmblack.png`, fs.F_OK, (err) => {
+      if (err) {
+        res.json({ stderr: "File Not Found" });
+        return;
+      }
+      res.download(`./zmblack.png`, `zmblack.png`);
+    });
+  } catch (err) {
+    console.error(`Error while downloading File `, err.message);
+    next(err);
+  }
+});
+
 app.use("/v1", router);
 
 /* Error handler middleware */
@@ -34,7 +55,11 @@ app.use((err, req, res, next) => {
   return;
 });
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
 
 // app.listen(port, async () => {
 //   console.log(`API listening at http://localhost:${port}`);
